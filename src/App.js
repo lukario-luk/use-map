1ce418b3740bcf7cced853304a877aaf08475597
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-03-05 16:55:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    cadastro:[
+      {
+        CompleteName:"Lucas de Souza Pinto",
+        email: "lucasdesouzapinto2000@gmail.com",
+        tell: "24 999883540",
+      },
+      {
+        CompleteName:"Maria Santos do Carmo",
+        email: "mariasantosc@gmail.com",
+        tell: "44 998624772",
+      },
+      {
+        CompleteName:"Pedro Wilhan Matos Costa",
+        email: "Pedrostone15@gmail.com",
+        tell: "47 999172539",
+      },
+      {
+        CompleteName:"Paulo Gustavo de Andrade",
+        email: "Paulogustavoandrade@gmail.com",
+        tell: "41 999528815",
+      },
+      {
+        CompleteName:"Jusé Santos Milhãn",
+        email: "jsm1111@gmail.com",
+        tell: "11 999132161",
+      },
+      {
+        CompleteName:"Paulo do Rodo da Grassa",
+        email: "Paulo_rg267@hotmail.com",
+        tell: "15 999812502",
+      },
+    ]
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Fixa de inscrição vai na web</h1>
+        <ul>
+          {this.state.cadastro.map((item)=>(
+            <li><h2>{item.CompleteName}</h2>
+            <h3>{item.email}</h3>
+            <h3>{item.tell}</h3></li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
